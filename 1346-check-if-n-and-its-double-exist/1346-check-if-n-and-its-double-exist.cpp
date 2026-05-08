94f973d7c2a6abeddf8852771a54fef633834b79
Abdulhaque <abdulhaquecg@gmail.com>
2026-05-08 18:55:54 +0530

    Time: 0 ms (100%), Space: 13.8 MB (40.35%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+       unordered_map<int,int> mp;
+       for(int num : arr){
+        if(mp.count(2*num)){
+            return true;
+        }
+        if(num%2==0 && mp.count(num/2)){
+        return true;
+        }
+        mp[num]++;
+       } 
+       return  false;
+    }
+};
