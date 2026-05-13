bc242a9cf8eec44ceef57b6a17c7f0637489aa7b
Abdulhaque <abdulhaquecg@gmail.com>
2026-05-13 12:00:51 +0530

    Time: 0 ms (100%), Space: 17.6 MB (74.67%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int X=0;
+        for(string op:operations){
+            if(op=="++X"||op=="X++" ){
+                X +=1;
+            }
+            else{
+                X -=1;
+            }
+        }
+        return X;
+    }
+};
