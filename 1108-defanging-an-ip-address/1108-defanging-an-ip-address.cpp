c724052be50a6368019ceed2e32bffe818441ee6
Abdulhaque <abdulhaquecg@gmail.com>
2026-05-15 08:09:54 +0530

    Time: 0 ms (100%), Space: 7.9 MB (11.65%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-    string add =  "";
+    string add="";
     for(int i=0; i<address.size(); i++){
         if(address[i]=='.'){
-         add +="[.]";    
+             add +="[.]";
         }
         else{
-        add +=address[i];
-
+            add +=address[i];
         }
     }
-
- return add;
+    return add;
     }
 };
