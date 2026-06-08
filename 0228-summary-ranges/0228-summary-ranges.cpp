29d767d3410f2f6a9911b6e023f4f147d6f5e7b7
Abdulhaque <abdulhaquecg@gmail.com>
2026-06-08 11:26:45 +0530

    Time: 0 ms (100%), Space: 9.2 MB (73.43%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string>arr;
+        
+        for(int i=0; i<nums.size(); i++){
+            int start= nums[i];
+            while(i+1<nums.size() && nums[i]+1== nums[i+1]){
+                i++;
+            }
+            int end = nums[i];
+
+            if(start==end){
+                arr.push_back(to_string(start));
+            }
+            else{
+                arr.push_back(to_string(start)+ "->"+ to_string(end));
+            }
+        }
+        return arr;
+        
+    }
+};
